7efa73cdb6529e3d168bf0a7d32c3abfb8e3ff78
Mikhail Myalitsyn <myalitsyn.ma@gmail.com>
2024-06-24 18:20:46 +0300

    fix(APP-48): messages paging response fix

--- services/message-paging-service/src/main/java/com/onesty/services/message/paging/service/MessageMapper.java
+++ services/message-paging-service/src/main/java/com/onesty/services/message/paging/service/MessageMapper.java
@@ -3,10 +3,23 @@ package com.onesty.services.message.paging.service;
 import com.onesty.api.core.message.ChatMessage;
 import com.onesty.services.message.paging.persistence.ChatMessageEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @Mapper(componentModel = "spring")
 public interface MessageMapper {
+    @Mapping(source = "id", target = "messageId")
+    @Mapping(source = "createdAt", target = "sentAt")
     ChatMessage toDto(ChatMessageEntity chatMessageEntity);
 
+    @Mapping(target = "messageId", ignore = true)
+    @Mapping(source = "sentAt", target = "createdAt")
+    @Mapping(target = "version", ignore = true)
     ChatMessageEntity toEntity(ChatMessage chatMessage);
+
+    default Timestamp map(Instant instant) {return Timestamp.from(instant);}
+
+    default Instant map(Timestamp timestamp) {return timestamp.toInstant();}
 }
