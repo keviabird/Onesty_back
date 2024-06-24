7ec984ba9e072846dd775495300342a95de9a1fc
Mikhail Myalitsyn <myalitsyn.ma@gmail.com>
2024-06-24 17:54:52 +0300

    feat(APP-48): mapper fix

--- services/message-service/src/main/java/com/onesty/services/message/service/ChatMessageMapper.java
+++ services/message-service/src/main/java/com/onesty/services/message/service/ChatMessageMapper.java
@@ -3,10 +3,22 @@ package com.onesty.services.message.service;
 import com.onesty.api.core.message.ChatMessage;
 import com.onesty.services.message.persistence.ChatMessageEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @Mapper(componentModel = "spring")
 public interface ChatMessageMapper {
+    @Mapping(source = "createdAt", target = "sentAt")
     ChatMessage toDto(ChatMessageEntity chatMessageEntity);
 
+    @Mapping(source = "messageId", target = "id")
+    @Mapping(source = "sentAt", target = "createdAt")
+    @Mapping(target = "version", ignore = true)
     ChatMessageEntity toEntity(ChatMessage chatMessage);
+
+    default Timestamp map(Instant instant) {return Timestamp.from(instant);}
+
+    default Instant map(Timestamp timestamp) {return timestamp.toInstant();}
 }
